b4cd0ea4952e1455e33776222aa809b42cac36a7
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2019-12-27 15:37:37 +0545

    here mongodb.atlas connected

--- test/appConfig.js
+++ test/appConfig.js
@@ -1,2 +1,10 @@
-export const uri = 'mongodb://localhost:27017/';
+const mongodbObject = {
+    user : 'rikesh',
+    password : 'testtest9849225111',
+
+};
+
+export const uri = `mongodb+srv://${mongodbObject.user}:${mongodbObject.password}@lakherikesh-8mon9.mongodb.net/test?retryWrites=true&w=majority`
+
+export const uri2 = 'mongodb://localhost:27017/';
 export const dbName = 'myproject';
