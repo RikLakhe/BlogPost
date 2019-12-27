492d4cd998ebe184cbf6f351b2706d95a4a4bfcf
Rikesh Lal Shrestha <rikeshlakhe@gmail.com>
2019-12-27 20:49:51 +0545

    Office (#4)
    * here mongodb.atlas connected
    
    * some error
    
    * work started in front end and back end testing
    
    * added in git ignore

---
+++ test/indexDB.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+import {uri, dbName} from './appConfig';
+
+import {addBlog,list,findById,update,remove} from './blogController'
+
+//mongoose.connect(uri, options);
+//Specifies which database to connect to and overrides any database specified in the connection string
+//useNewUrlParser flag to allow users to fall back to the old parser if they find a bug in the new parser
+mongoose
+    .connect(uri, {
+        // dbName: dbName,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log("DB Connected"))
+    .catch(err => console.log('error'));
+
+// CRUD = CREATE
+// addBlog(
+// {
+//     "body": {
+//         "title": "banana",
+//         "author": "apple-author",
+//         "body": "This is the test. blog posts. this is the second one."
+//     }})
+
+// CRUD = READ
+// list()
+// findById("5e048e799bdbdf0a20bff24b")
+
+// CRUD = UPDATE
+// update("5e048e799bdbdf0a20bff24b",{
+//     "body": {
+//         "title": "banana",
+//         "author": "apple-author",
+//         "body": "This is the test. blog posts. this is the second one."
+//     }})
+
+// CRUD = DESTROY
+// remove("5e048e799bdbdf0a20bff24b");
