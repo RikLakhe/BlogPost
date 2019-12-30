91fb70e3d79ad91a7acb0860f74362c913e1bb93
Rikesh Lal Shrestha <rikeshlakhe@gmail.com>
2019-12-30 17:57:12 +0545

    Office (#9)
    * here mongodb.atlas connected
    
    * some error
    
    * work started in front end and back end testing
    
    * added in git ignore
    
    * here working
    
    * worked in layouts
    
    * worked on route done
    
    * finals of the backend planning
    
    * working in front end
    
    * footer fixed

--- fronttest/src/component/Layout/Footer/index.js
+++ fronttest/src/component/Layout/Footer/index.js
@@ -1,20 +1,36 @@
 import React,{Fragment} from 'react'
+import {Link} from "react-router-dom";
 import styled from "styled-components";
 
 import {appConfig} from '../../../constants/AppConfig.js'
 
 const AppFooter = props =>{
     const Footer = styled.footer`
+        margin: 0;
+        padding: 0;
         position: fixed;
-        bottom:0;
-        background: ${appConfig.color.secondary};
-        width: 100%;
-        padding: 10px;
+        bottom: 0;
+        right: 0;
+        width:100%
+        background-color: #bfbfbf;
+        
+        div{
+            float: right;
+        }
+        
+        a{
+            text-decoration: none;
+            color:black;
+            font-size:12px;
+            padding-right:10px;
+        }
     `;
 
     return(
         <Footer>
-            this is footer
+            <div>
+                <Link to={"/"}>©{" "}{appConfig.year},{appConfig.brand}. All Rights Reserved.</Link>
+            </div>
         </Footer>
     )
 };
